b151efe4f7b69369be39b5ef5298e2dc53eeb810
Nikhil Reddy <35322884+sainikhil1605@users.noreply.github.com>
2023-06-14 20:29:54 -0500

    Added BE Initial code and added JWT Support

---
+++ lms-server/src/main/java/com/lms/api/LmsServerApplication.java
@@ -0,0 +1,13 @@
+package com.lms.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsServerApplication.class, args);
+	}
+
+}
